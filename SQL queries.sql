d6d565a677c4df16b57aa1da1da5f3b1a16099e6
Roee Tsahi <Roeit26@gmail.com>
2026-08-22 03:19:40 +0300

    Create SQL queries.sql

---
+++ SQL queries.sql
@@ -0,0 +1,21 @@
+--  Missing a LINE_ITEM_ID, this CTE partitions by ALL 23 columns to safely collapse  a 14% exact-duplicate logging bug. This surgical deduplication is critical to  prevent severe revenue inflation and protect overall KPI integrity. 
+CREATE TABLE orders_dataset_clean_no_duplicates AS
+SELECT *
+FROM (
+    SELECT *,
+        ROW_NUMBER() OVER (
+            PARTITION BY
+                USER_ID, ORDER_ID, PURCHASE_DATE, PURCHASE_MONTH_YEAR,
+                SHIP_DATE, DELIVERY_DATE, REFUND_DATE, REFUNDED,
+                DAYS_TO_SHIP, TRANSIT_TIME, RETURN_WINDOW,
+                PRODUCT_NAME, PRODUCT_ID, USD_PRICE, LOCAL_PRICE,
+                CURRENCY, PURCHASE_PLATFORM, MARKETING_CHANNEL,
+                ACCOUNT_CREATION_METHOD, COUNTRY_CODE, REGION,
+                LOYALTY_PROGRAM, CREATED_ON
+            ORDER BY ORDER_ID
+        ) as rn
+    FROM orders_final
+) sub
+WHERE rn = 1;
+
+ALTER TABLE orders_dataset_clean_no_duplicates DROP COLUMN rn;
